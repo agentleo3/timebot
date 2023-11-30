f6fae079317e484cdac193d0e695af8105388113
Leo <leo.logvinov@gmail.com>
2023-11-30 17:59:08 +0300

    сделали запрос времени к api

--- webhook.py
+++ webhook.py
@@ -15,12 +15,27 @@ def webhook():
     return make_response(jsonify(get_result()))
 
 
+def get_time(latitude, longitude):
+    url = f'https://timeapi.io/api/Time/current/coordinate?latitude={latitude}&longitude={longitude}'
+    response = requests.get(url).json()
+    time = response["time"]
+    date = response["date"]
+    return f"{time}, {date}"
+
+
 def get_result():
     # извлечение параметра
     req = request.get_json(force=True)
     print(req)
+    current_time = get_time(38.9, -77.03)
+    print(current_time)
+    return {
+        "fulfillmentText": current_time
+    }
+
 
 
 app.run(host='0.0.0.0', port=81)
+
 
 
